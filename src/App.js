a29ce2b7a842d1de877602dc09a83a42666709c6
NMohanGandhi <mohanyadav8374@gmail.com>
2025-05-26 19:22:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import CoreValues from './Componenets/Corevalues';
+import Footer from './Componenets/Footer';
+import Header from './Componenets/Header';
+import HeroSection from './Componenets/Herocomponent';
+import Visionmission from './Componenets/visionmission';
+import HowToDonate from './Component1/HowToDonate';
+import SuccessStories from './Component1/SuccessStories';
+import VolunteerPage from './Component2/VolunteerPage';
+import FooterGoodgive from './Component2/FooterGoodgive';
+// import Header from './header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <HeroSection/>
+     <Visionmission/>
+     <CoreValues/>
+     <Footer/>
+     <HowToDonate/>
+     <SuccessStories/>
+     <VolunteerPage/>
+     <FooterGoodgive/>
+
     </div>
   );
 }
